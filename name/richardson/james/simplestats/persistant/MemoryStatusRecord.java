04a70367dc385aec85919be029fc074f75d258d3
James Richardson <me+github@james.richardson.name>
2011-11-03 22:13:18 +0000

    - more code clean up

--- name/richardson/james/simplestats/persistant/MemoryStatusRecord.java
+++ name/richardson/james/simplestats/persistant/MemoryStatusRecord.java
@@ -3,36 +3,13 @@
  * 
  * MemoryStatusRecord.java is part of SimpleStats.
  * 
- * SimpleStats is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the Free 
- * Software Foundation, either version 3 of the License, or (at your option) 
- * any later version.
+ * SimpleStats is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
- * SimpleStats is distributed in the hope that it will be useful, but WITHOUT ANY 
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
- * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * SimpleStats is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License 
+ * You should have received a copy of the GNU General Public License
  * along with SimpleStats.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-/* 
-Copyright 2011 James Richardson.
-
-This file is part of SimpleStats.
-
-SimpleStats is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-SimpleStats is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with SimpleStats.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 package name.richardson.james.simplestats.persistant;
 
@@ -46,58 +23,58 @@ import com.avaje.ebean.validation.NotNull;
 @Table(name = "simplestats_memory")
 public class MemoryStatusRecord extends Record {
 
-	@Id
-    private long createdAt;
-	
-	@NotNull
-    private int memoryTotal;
-    
-    @NotNull
-    private int memoryUsed;
-    
-    @NotNull
-    private int memoryFree;
-    
-    @NotNull
-    private int memoryMax;
+  @Id
+  private long createdAt;
 
-	public void setCreatedAt(long createdAt) {
-		this.createdAt = createdAt;
-	}
+  @NotNull
+  private int  memoryTotal;
 
-	public long getCreatedAt() {
-		return createdAt;
-	}
+  @NotNull
+  private int  memoryUsed;
 
-	public void setMemoryUsed(int memoryUsed) {
-		this.memoryUsed = memoryUsed;
-	}
+  @NotNull
+  private int  memoryFree;
 
-	public int getMemoryUsed() {
-		return memoryUsed;
-	}
+  @NotNull
+  private int  memoryMax;
 
-	public void setMemoryFree(int memoryFree) {
-		this.memoryFree = memoryFree;
-	}
+  public void setCreatedAt(long createdAt) {
+    this.createdAt = createdAt;
+  }
 
-	public int getMemoryFree() {
-		return memoryFree;
-	}
+  public long getCreatedAt() {
+    return createdAt;
+  }
 
-	public void setMemoryTotal(int memoryTotal) {
-		this.memoryTotal = memoryTotal;
-	}
+  public void setMemoryUsed(int memoryUsed) {
+    this.memoryUsed = memoryUsed;
+  }
 
-	public int getMemoryTotal() {
-		return memoryTotal;
-	}
+  public int getMemoryUsed() {
+    return memoryUsed;
+  }
 
-	public void setMemoryMax(int memoryMax) {
-		this.memoryMax = memoryMax;
-	}
+  public void setMemoryFree(int memoryFree) {
+    this.memoryFree = memoryFree;
+  }
 
-	public int getMemoryMax() {
-		return memoryMax;
-	}
+  public int getMemoryFree() {
+    return memoryFree;
+  }
+
+  public void setMemoryTotal(int memoryTotal) {
+    this.memoryTotal = memoryTotal;
+  }
+
+  public int getMemoryTotal() {
+    return memoryTotal;
+  }
+
+  public void setMemoryMax(int memoryMax) {
+    this.memoryMax = memoryMax;
+  }
+
+  public int getMemoryMax() {
+    return memoryMax;
+  }
 }
--- name/richardson/james/simplestats/persistant/PlayerCountRecord.java
+++ name/richardson/james/simplestats/persistant/PlayerCountRecord.java
@@ -15,34 +15,12 @@
  * You should have received a copy of the GNU General Public License 
  * along with SimpleStats.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-/*
- Copyright 2011 James Richardson.
-
- This file is part of SimpleStats.
-
- SimpleStats is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- SimpleStats is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with SimpleStats.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 package name.richardson.james.simplestats.persistant;
-
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import name.richardson.james.simplestats.SimpleStats;
 
 import com.avaje.ebean.validation.NotNull;
 
@@ -85,10 +63,6 @@ public class PlayerCountRecord extends Record {
 
   public int getPlayerMax() {
     return playerMax;
-  }
-
-  public static List<PlayerCountRecord> findAllBetween(long start, long end) {
-    return SimpleStats.getDb().find(PlayerCountRecord.class).where().between("created_at", start, end).findList();
   }
 
 }
